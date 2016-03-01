5db16a46005563c0a5eac45f1d7ebbd73e2b1294
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-03-01 08:09:07 -0500

    machine provisioning example

--- hpe-cookbook/recipes/default.rb
+++ hpe-cookbook/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+require 'chef/provisioning'
+
+num_webservers = 2
+
+machine_batch do
+	1.upto(num_webservers) do |i|
+	  machine "luigi#{i}" do
+	   recipe 'htop'
+	   recipe 'nginx'
+	   tag 'test_provisioning'
+	  end
+	end
+end
+
